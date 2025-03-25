d4f95658d00cf10ffb8917e79bae5b291b9e1449
Yash-Ghule <yashghule24@gmail.com>
2025-03-25 13:37:23 +0000

    dataTypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+// code readability is priority
+
+let name = "yash"
+let age = 15
+let loggin = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "yash");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
