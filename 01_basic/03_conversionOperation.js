8fef57ffc84ea5519ca9ae445a7c7d210b4b8c35
Yash-Ghule <yashghule24@gmail.com>
2025-03-25 14:12:56 +0000

    conversion

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,32 @@
+//let score = "33abc"
+//let score = null
+let score = undefined
+
+console.log(typeof score);
+
+let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+//let isLoggedIn = 1
+//let isLoggedIn = ""
+let isLoggedIn = "yash"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "yash" => true
+
+let someNum = 54
+
+let stingNumber = String(someNum)
+console.log(stingNumber);
+console.log(typeof stingNumber);
+
