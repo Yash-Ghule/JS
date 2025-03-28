f4a617230945855bf1a76643d41b42461c8f8614
Yash-Ghule <yashghule24@gmail.com>
2025-03-28 04:32:31 +0000

    operation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,3 +1,4 @@
+/*
 //let score = "33abc"
 //let score = null
 let score = undefined
@@ -29,4 +30,37 @@ let someNum = 54
 let stingNumber = String(someNum)
 console.log(stingNumber);
 console.log(typeof stingNumber);
+*/
+
+// *********************** Operations ***************************
+
+let value = 3
+let negValue = -value 
+console.log(negValue);
+
+// Basic operations are also there like:
+// Addition, Subtraction, Multiplication, division, remainder, power
+
+
+let str1 = "hello"
+let str2 = " yash"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2); // o/p = 12
+console.log(1 + "2"); // o/p = 12
+console.log("1" + 2 + 2); // o/p = 122
+console.log(1 + 2 + "2"); // o/p = 32
+
+console.log(+true); // 1
+console.log(+""); // 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter); // o/p = 101
 
