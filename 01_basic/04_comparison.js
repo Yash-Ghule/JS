a3700e410cc2cb59fb028ec5f0059c8e36194ad7
Yash-Ghule <yashghule24@gmail.com>
2025-03-28 04:40:33 +0000

    comparison

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+
+// === it strictly checks 
+
+console.log("2" = 2); // this will work
+console.log("2" == 2); // this will not work
+console.log("2" === 2); // this will not work
