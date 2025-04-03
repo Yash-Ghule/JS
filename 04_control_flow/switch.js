da2b7bc93158ea611c6cdd2a400de18dcce37017
Yash-Ghule <yashghule24@gmail.com>
2025-04-03 04:35:49 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,54 @@
+// Syntax
+// switch(key){
+//     case value:
+
+//     break;
+
+//     default:
+//         break;
+// }
+
+
+/*
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;              // we use break because it executes everything after the condition matches EXCEPT for default it is not executed 
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("Mar");
+        break;
+    case 4:
+        console.log("Apr");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+*/
+
+
+const month = "apr"
+switch (month) {
+    case "jan":
+        console.log("Jan");
+        break;              // we use break because it executes everything after the condition matches EXCEPT for default it is not executed 
+    case "feb":
+        console.log("Feb");
+        break;
+    case "mar":
+        console.log("Mar");
+        break;
+    case "apr":
+        console.log("Apr");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
