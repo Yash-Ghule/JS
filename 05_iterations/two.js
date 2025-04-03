33f3ad63f871cb12d126bc8cd1f7fa59bcc8e3f7
Yash-Ghule <yashghule24@gmail.com>
2025-04-03 08:25:58 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,25 @@
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+// do while loop
+
+let score = 1
+// let score = 11 // here it prints 'Score is 11' as first task is carried out then condition is checked
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
